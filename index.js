fd1b12916057dea24a1bf56d445d10012d7d0f27
Fwy13_ <136143802+fwy13@users.noreply.github.com>
2024-11-09 12:17:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ import fetch from "node-fetch";
 const PORT = process.env.PORT || 4000
 
 createServer(async (req, res) => {
-    try {
-        const imageUrl = req.url.slice(1);
-        if (imageUrl !== "") {
-            const response = await fetch(imageUrl, {
-                headers: {
-                    referer: "https://www.nettruyenmax.com",
-                },
-            });
-            console.log(`Succesful a request<3`)
-            res.writeHead(response.status, response.headers);
-            response.body.pipe(res);
-        }
-    } catch (err) {
-        console.error(err);
-        res.writeHead(500);
-        res.end(err.toString());
-    }
+    res.write("I'm alive");
+    res.end();
 }).listen(PORT, () => {
     console.log(
         `Image proxy server running. Access it at: http://localhost:${PORT}/`
